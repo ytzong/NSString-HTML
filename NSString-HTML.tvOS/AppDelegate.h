8f76c5ba52b1de4bfad8c27c80f45a1e00838039
ytzong <ytzong7@gmail.com>
2016-02-29 14:28:14 +0800

    +tvOS

---
+++ NSString-HTML.tvOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSString-HTML.tvOS
+//
+//  Created by ytzong on 29/2/16.
+//  Copyright © 2016年 No Zebra Network. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSString-HTML.tvOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSString-HTML.tvOS
+//
+//  Created by ytzong on 29/2/16.
+//  Copyright © 2016年 No Zebra Network. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
